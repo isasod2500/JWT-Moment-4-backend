cb7f673a6a30650f9f175b2fceded6bb0d039b05
isso2500 <isso2500@student.miun.se>
2026-05-14 14:05:24 +0200

    Added extra-secret webpage for only signed in users, authenticating token.

--- server.js
+++ server.js
@@ -23,6 +23,34 @@ app.get("/signedin", authenticateToken, (req, res) => {
     res.json({ message: `Admin route, protected` })
 })
 
+app.get("/secret", authenticateToken, async (req, res) => {
+    try {
+        const { username, token } = req.body
+
+        if (!username || !token) {
+            return res.status(403).json({ error: `Access denied. Token or username missing.` })
+        }
+
+        const decrypt = jwt.verify(token, JWT_SECRET_KEY);
+
+        const user = await User.findById(decrypt.id)
+
+        const response = res.json({
+            username: user.username,
+            firstname: user.firstname,
+            surname: user.surname,
+            email: user.email,
+            created: user.created
+        })
+
+        console.log(response)
+    } catch (err) {
+        res.status(401).json({
+            error: err.message
+        });
+    }
+})
+
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(" ")[1]
